c47b0ca4c6815a206a7779c12bcd2d0e685f3a3f
peter.van.rijn@little-world.nl <peter.van.rijn@little-world.nl>
2019-10-23 16:19:16 +0200

    draw rect

--- src/io/littleworld/Scribble.java
+++ src/io/littleworld/Scribble.java
@@ -1,8 +1,21 @@
 package io.littleworld;
 
-public class Scribble {
+import javax.swing.*;
+import java.awt.*;
+
+public class Scribble extends JFrame {
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        g.drawRect(20, 20, 100, 100);
+    }
 
     public static void main(String[] args) {
 	// write your code here
+        Scribble scribble = new Scribble();
+        scribble.setSize(400, 400);
+        scribble.setVisible(true);
+        scribble.setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
 }
